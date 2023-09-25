caa799a12eee8eb88d4f9292e11bb078af4826ba
Johnmani Das <johnmani.das@tesco.com>
2023-09-25 16:28:01 +0530

    main

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	// ... other imports
 	"github.com/traefik/traefik/v2/pkg/config/dynamic"
 )
 
 func init() {
-	// Register your plugin type
 	dynamic.Add("custommetrics", func() interface{} {
-		return &CustomMetrics{}
+		return &CustomMetricsMiddleware{}
 	})
 }
